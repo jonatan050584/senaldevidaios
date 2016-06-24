4d44a77d6c5b7a35f00657a1a6e1427d29587c41
Jonatán López <jonatan_angelo@hotmail.com>
2016-06-24 18:17:01 -0500

    va

--- www/js/geoposicion.js
+++ www/js/geoposicion.js
@@ -6,23 +6,80 @@ var Geoposicion = function(){
 
 
     this.iniciar = function(){
-        console.log(">> Inicia el timer de posición FRONT");
+        /*console.log(">> Inicia el timer de posición FRONT");
         this.accion();
-        geoint = setInterval(geoposicion.accion,60*1000*5);
+        geoint = setInterval(geoposicion.accion,60*1000*5);*/
+
+
+        if(usuario.grupo!=undefined && usuario.grupo!=null){
+            var callbackFn = function(location) {
+                //console.log('[js] BackgroundGeoLocation callback:  ' + location.latitude + ',' + location.longitude);
+                if(backtimer!=null) clearInterval(backtimer);
+
+                var posicion = location.latitude+","+location.longitude;
+                console.log(">> Enviando posicion desde background: "+posicion);
+                posicion = enc.encriptar(posicion,usuario.grupo.llave);
+
+
+
+                socket.emit("algrupo",{ac:"posicion",grupo:usuario.grupo.llave,id:usuario.id,posicion:posicion,de:"back"});
+
+                
+                backgroundGeoLocation.finish();
+
+                backtimer = setInterval(function(){
+                    navigator.geolocation.getCurrentPosition(function(pos){
+                            tempos = pos;
+                    });
+                },5000);
+
+            };
+
+            var failureFn = function(error) {
+                console.log('BackgroundGeoLocation error');
+            };
+
+
+
+            backtimer = setInterval(function(){
+                navigator.geolocation.getCurrentPosition(function(pos){
+                        tempos = pos;
+                });
+            },5000);
+
+            sendtimer = setInterval(function(){
+                var posicion = tempos.coords.latitude+","+tempos.coords.longitude;
+                console.log(">> Enviando posicion desde background timer: "+posicion);
+                posicion = enc.encriptar(posicion,usuario.grupo.llave);
+
+                socket.emit("algrupo",{ac:"posicion",grupo:usuario.grupo.llave,id:usuario.id,posicion:posicion,de:"backtimer"});
+            },1000*60*5);
+
+            // BackgroundGeoLocation is highly configurable. See platform specific configuration options
+            backgroundGeoLocation.configure(callbackFn, failureFn, {
+                desiredAccuracy: 10,
+                stationaryRadius: 20,
+                activityType: 'Other', 
+                distanceFilter: 30,
+                debug: false, // <-- enable this hear sounds for background-geolocation life-cycle.
+                stopOnTerminate: false, // <-- enable this to clear background location settings when the app terminates
+            });
+
+            // Turn ON the background-geolocation system.  The user will be tracked whenever they suspend the app.
+            backgroundGeoLocation.start();
+        }
+        
 
         
 
     }
 
     this.accion = function(){
-        //console.log("usuario grupo:");
-        //console.log(usuario.grupo);
-            //console.log("aca 1");
+      
             if(usuario.grupo!=undefined && usuario.grupo!=null){
-                //console.log("aca 2");
+               
                 navigator.geolocation.getCurrentPosition(function(pos){
-                    //console.log("accion pos: ");
-                    //console.log(pos);
+                   
                     if(usuario.grupo!=undefined && usuario.grupo!=null){
                         var posicion = pos.coords.latitude+","+pos.coords.longitude;
                         console.log(">> Enviando posicion desde front: "+posicion);
@@ -39,6 +96,7 @@ var Geoposicion = function(){
                                 'message: ' + error.message + '\n');
                 });
             }
+
     }
 
 }
--- www/js/index.js
+++ www/js/index.js
@@ -97,8 +97,8 @@ var app = {
         //alert("resume");
         //alert(1);
         if(usuario!=undefined && usuario.grupo!=undefined && usuario.grupo!=null){
-            backgroundGeoLocation.stop();
-            clearInterval(backtimer);
+            //backgroundGeoLocation.stop();
+            //clearInterval(backtimer);
         }
         //comprobarVersion();
 
@@ -141,58 +141,7 @@ var app = {
     },
     onDevicePause:function(){
 
-        if(usuario!=undefined && usuario.grupo!=undefined && usuario.grupo!=null){
-            var callbackFn = function(location) {
-                //console.log('[js] BackgroundGeoLocation callback:  ' + location.latitude + ',' + location.longitude);
 
-
-                var posicion = location.latitude+","+location.longitude;
-                console.log(">> Enviando posicion desde background: "+posicion);
-                posicion = enc.encriptar(posicion,usuario.grupo.llave);
-
-
-
-                socket.emit("algrupo",{ac:"posicion",grupo:usuario.grupo.llave,id:usuario.id,posicion:posicion,de:"back"});
-
-                
-                backgroundGeoLocation.finish();
-
-                backtimer = setInterval(function(){
-                    navigator.geolocation.getCurrentPosition(function(pos){
-                            //console.log("interval back");
-                            //console.log(pos);
-                            tempos = pos;
-                    });
-                },5000);
-
-                sendtimer = setInterval(function(){
-                    var posicion = tempos.coords.latitude+","+tempos.coords.longitude;
-                     console.log(">> Enviando posicion desde background timer: "+posicion);
-                    posicion = enc.encriptar(posicion,usuario.grupo.llave);
-
-                    socket.emit("algrupo",{ac:"posicion",grupo:usuario.grupo.llave,id:usuario.id,posicion:posicion,de:"backtimer"});
-                },1000*60*5);
-
-            };
-
-            var failureFn = function(error) {
-                console.log('BackgroundGeoLocation error');
-            };
-
-            // BackgroundGeoLocation is highly configurable. See platform specific configuration options
-            backgroundGeoLocation.configure(callbackFn, failureFn, {
-                desiredAccuracy: 10,
-                stationaryRadius: 20,
-                activityType: 'Other', 
-                distanceFilter: 30,
-                debug: false, // <-- enable this hear sounds for background-geolocation life-cycle.
-                stopOnTerminate: false, // <-- enable this to clear background location settings when the app terminates
-            });
-
-            // Turn ON the background-geolocation system.  The user will be tracked whenever they suspend the app.
-            backgroundGeoLocation.start();
-        }
-        
         
         
     },
